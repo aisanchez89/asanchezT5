ad4d00ebc24aff29a514263aad8359adf7483ed1
USUARIO <USUARIO@DESKTOP-FUCAENO>
2023-07-03 01:54:20 -0500

    Agregar archivos de proyecto.

---
+++ asanchezT5/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace asanchezT5
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://192.168.100.65/wsisrael/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<Ruta> post;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<Ruta> listaPost = JsonConvert.DeserializeObject<List<Ruta>>(contenido);
+            post = new ObservableCollection<Ruta>(listaPost);
+            RutasRegistradas.ItemsSource = post;
+        }
+    }
+}
---
+++ asanchezT5/Ruta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asanchezT5
+{
+    public class Ruta
+    {
+        public int Id_ruta { set; get; }
+        public string Sector { set; get; }
+        public string Barrio { set; get; }
+        public string Calle_principal { set; get; }
+        public string Numeracion { set; get; }
+        public string Calle_secundaria { set; get; }
+    }
+}
